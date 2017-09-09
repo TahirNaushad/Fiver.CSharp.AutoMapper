06c7779ab7c42796ae656afad6a6960510076807
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-09 15:38:53 +0100

    Add project files.

---
+++ Fiver.CSharp.AutoMapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fiver.CSharp.AutoMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
